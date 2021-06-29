88d9586183bf94a32679a51c31a6094927d2eea6
Shawn <shawngmc@gmail.com>
2021-06-29 17:15:52 -0400

    Logo and UI Text Updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,9 +15,9 @@ module.exports = {
   trailingSlash: true,
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'Crypto Cheatsheet',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Crypto Cheatsheet Logo',
         src: 'img/logo.svg',
       },
       items: [
@@ -29,7 +29,7 @@ module.exports = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/shawngmc/crypto-cheatsheet',
           label: 'GitHub',
           position: 'right',
         },
@@ -48,23 +48,6 @@ module.exports = {
           ],
         },
         {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
           title: 'More',
           items: [
             {
@@ -73,12 +56,12 @@ module.exports = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/shawngmc/crypto-cheatsheet',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Shawn McNaughton, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: lightCodeTheme,
@@ -93,13 +76,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/shawngmc/crypto-cheatsheet/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/shawngmc/crypto-cheatsheet/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),

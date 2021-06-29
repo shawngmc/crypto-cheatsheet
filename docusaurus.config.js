04b22a3ee266038f675801418470465e45d67aa7
Shawn <shawngmc@gmail.com>
2021-06-29 17:07:25 -0400

    config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,13 +5,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://shawngmc.github.io',
+  baseUrl: '/crypto-cheatsheet/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'shawngmc', // Usually your GitHub org/user name.
+  projectName: 'crypto-cheatsheet', // Usually your repo name.
+  trailingSlash: true,
   themeConfig: {
     navbar: {
       title: 'My Site',

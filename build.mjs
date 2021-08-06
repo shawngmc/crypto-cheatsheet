3003c7ce634e46e167718d4f096363786dbe7b43
Shawn <shawngmc@gmail.com>
2021-08-06 09:10:50 -0400

    temporary version

--- build.mjs
+++ build.mjs
@@ -20,13 +20,38 @@ let newDom = new JSDOM(skeletonHtml);
 let newDoc = newDom.window.document;
 let sectionTemplateHtml = fs.readFileSync('templates/section-template.html', 'utf-8');
 
+let cleanTable = function(table) {
+    // Remove existing headers
+    table.querySelector('thead').remove();
+    let tbody = table.querySelector('tbody');
+    tbody.querySelectorAll('th').forEach((th) => {
+        th.remove();
+    })
+
+    // Clean styles
+    table.className = "";
+    table.querySelectorAll('tr').forEach((tr) => {
+        tr.className = "";
+        tr.removeAttribute("style");
+    })
+    table.querySelectorAll('td').forEach((td) => {
+        td.className = "";
+        td.removeAttribute("dir");
+    })
+
+    let header = JSDOM.fragment("<thead></thead>");
+
+}
+
 config.sections.forEach((section) => {
-    console.log(section);
+    console.log(`Building ${section.title}...`);
 
     // Read child doc
     let childHtml = fs.readFileSync(`./raw-sheets/${section.filename}`, 'utf-8');
     let childDom = JSDOM.fragment(childHtml);
     let childTable = childDom.querySelector('table');
+
+    cleanTable(childTable);
 
     // Prep Content Section
     let childId = section.title.replace(" ", "-");
@@ -43,7 +68,7 @@ config.sections.forEach((section) => {
     newDoc.querySelector('article').appendChild(newChildDom);
 
     // Insert TOC entry
-    let newNav = JSDOM.fragment(`<a href="#${childId}">${section.title}</a><br>`);
+    let newNav = JSDOM.fragment(`<a href="#${childId}">${section.title}</a>`);
     newDoc.querySelector('nav').appendChild(newNav);
 })
 // Write HTML
